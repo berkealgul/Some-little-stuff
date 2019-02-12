7bd8a1fa62fef2c8934d17d900e50d0902aa792e
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2019-02-12 17:26:49 +0300

    Add files via upload

---
+++ Labirent_ Oluşturma_Çözme/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Labirent__Oluşturma_Çözme
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Maze = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hesaplayıcı = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Maze
+            // 
+            this.Maze.Location = new System.Drawing.Point(21, 24);
+            this.Maze.Name = "Maze";
+            this.Maze.Size = new System.Drawing.Size(500, 450);
+            this.Maze.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(539, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(536, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(536, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "__";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(536, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Çözme Süresi(sn):";
+            // 
+            // hesaplayıcı
+            // 
+            this.hesaplayıcı.DoWork += new System.ComponentModel.DoWorkEventHandler(this.hesaplayıcı_DoWork);
+            this.hesaplayıcı.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.hesaplayıcı_RunWorkerCompleted);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(668, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Maze);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "form1";
+            this.Text = "Labirent Algoritması";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Panel Maze;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker hesaplayıcı;
+    }
+}
+
---
+++ Labirent_ Oluşturma_Çözme/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Labirent__Oluşturma_Çözme
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hoşgeldiniz";
+        }
+
+
+        Random rnd = new Random();
+        Pen p = new Pen(Color.Black, 2);
+        SolidBrush sbg = new SolidBrush(Color.Green);
+        SolidBrush sbr = new SolidBrush(Color.Red);
+        SolidBrush sbb = new SolidBrush(Color.Blue);
+        SolidBrush sbbl = new SolidBrush(Color.Black);
+        Stopwatch sw = new Stopwatch();
+        bool başarılı = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!hesaplayıcı.IsBusy)
+            {
+                label1.Text = "İşleniyor...";
+                button1.Text = "---";
+                label3.Text = "__";
+                hesaplayıcı.RunWorkerAsync();
+            }   
+        }
+
+        private void hesaplayıcı_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            sw.Start();
+
+            Graphics f = this.CreateGraphics();
+            f.DrawRectangle(p, 20, 23, 502, 452);
+            Graphics g = Maze.CreateGraphics();
+            g.Clear(Color.Gray);
+            int w = Maze.Width / 50;
+            int h = Maze.Height / 50;
+            List<Rectangle> dolu = new List<Rectangle>();
+            List<Rectangle> boş = new List<Rectangle>();
+            float c;
+            Rectangle start = new Rectangle(0, 0, w, h);
+            Rectangle finish = new Rectangle(Maze.Width - w, Maze.Height - h, w, h);
+            g.FillRectangle(sbg, finish);
+
+            for (int x = 0; x < Maze.Width; x += w)
+            {
+                for (int y = 0; y < Maze.Height; y += 2 * h)
+                {
+                    double num = rnd.NextDouble();
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        Rectangle r = new Rectangle(x, y + h * i, w, h);
+                        if ((x == 0 && y == 0) || (x == Maze.Width - w && y == Maze.Height - 2 * h))
+                        {
+                            g.DrawRectangle(p, r);
+                            boş.Add(r);
+                        }
+                        else
+                        {
+                            if ((x > 20 * w && y > 20 * h) || (x < 10 * w && y < 10 * h))
+                                c = 0.22f;
+                            else
+                                c = 0.35f;
+                            if (num <= c)
+                            {
+                                dolu.Add(r);
+                                g.FillRectangle(sbbl, r);
+                            }
+                            else
+                            {
+                                boş.Add(r);
+                                g.DrawRectangle(p, r);
+                            }
+                        }
+                    }
+                }
+            }  //creates a maze (it become a mine field but anyways)
+
+            Rectangle current = start;
+            List<Rectangle> önceki = new List<Rectangle>();
+            List<double> uzaklık = new List<double>();
+            List<Rectangle> aday = new List<Rectangle>();
+            int ind;
+
+            while (current != finish) //solves created maze
+            {
+                Rectangle r;
+                g.FillRectangle(sbb, current);
+
+
+                if (boş.Contains(r = new Rectangle(current.X, current.Y - h, w, h)) && !dolu.Contains(r = new Rectangle(current.X, current.Y - h, w, h)))
+                {
+                    uzaklık.Add(Math.Sqrt(Math.Pow(finish.X - r.X, 2) + Math.Pow(finish.Y - r.Y, 2)));
+                    aday.Add(r);
+                }
+                if (boş.Contains(r = new Rectangle(current.X + w, current.Y, w, h)) && !dolu.Contains(r = new Rectangle(current.X + w, current.Y, w, h)))
+                {
+                    uzaklık.Add(Math.Sqrt(Math.Pow(finish.X - r.X, 2) + Math.Pow(finish.Y - r.Y, 2)));
+                    aday.Add(r);
+                }
+                if (boş.Contains(r = new Rectangle(current.X, current.Y + h, w, h)) && !dolu.Contains(r = new Rectangle(current.X, current.Y + h, w, h)))
+                {
+                    uzaklık.Add(Math.Sqrt(Math.Pow(finish.X - r.X, 2) + Math.Pow(finish.Y - r.Y, 2)));
+                    aday.Add(r);
+                }
+                if (boş.Contains(r = new Rectangle(current.X - w, current.Y, w, h)) && !dolu.Contains(r = new Rectangle(current.X - w, current.Y, w, h)))
+                {
+                    uzaklık.Add(Math.Sqrt(Math.Pow(finish.X - r.X, 2) + Math.Pow(finish.Y - r.Y, 2)));
+                    aday.Add(r);
+                }
+
+                dolu.Add(current);
+
+                if (aday.Count == 0)
+                {
+                    if (önceki.Count > 0)
+                        current = önceki[önceki.Count - 1];
+                    else
+                        break;
+                    önceki.RemoveAt(önceki.Count - 1);
+                }
+                else
+                {
+                    ind = 0;
+                    for (int i = 0; i < uzaklık.Count; i++)
+                    {
+                        if (uzaklık[i] < uzaklık[ind])
+                            ind = i;
+                    }
+                    current = aday[ind];
+                    aday.Clear();
+                    uzaklık.Clear();
+                    önceki.Add(current);
+                }
+
+                g.FillRectangle(sbr, current);
+                Thread.Sleep(50);
+            }
+
+            if (current == finish)
+            {
+                başarılı = true;
+                g.FillRectangle(sbg, start);
+                for (int i = 0; i < önceki.Count; i++)
+                {
+                    g.FillRectangle(sbg, önceki[i]);
+                }
+            }
+            else
+                başarılı = false;
+            
+        }
+
+        private void hesaplayıcı_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            sw.Stop();
+           
+            if (başarılı)
+                label1.Text = "Labirent Çözüldü";
+            else
+                label1.Text = "Yol Bulunamadı";
+
+            button1.Text = "Tekrar";
+            label3.Text = Convert.ToString((float)sw.ElapsedMilliseconds / 1000);
+            sw.Reset();
+
+        }
+    }
+}
