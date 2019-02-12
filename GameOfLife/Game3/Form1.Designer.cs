a34a8f478c98aa3278186b9bf49501a47bcd32a2
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2019-02-12 18:40:24 +0300

    Add files via upload

---
+++ GameOfLife/Game3/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Game3
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.hesaplayıcı = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hesaplayıcı
+            // 
+            this.hesaplayıcı.WorkerSupportsCancellation = true;
+            this.hesaplayıcı.DoWork += new System.ComponentModel.DoWorkEventHandler(this.hesaplayıcı_DoWork);
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(691, 470);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "AnaSayfa";
+            this.Text = "Game Of Life";
+            this.Load += new System.EventHandler(this.AnaSayfa_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker hesaplayıcı;
+    }
+}
+
---
+++ GameOfLife/Game3/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game3
+{
+    public partial class AnaSayfa : Form
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AnaSayfa_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            k = new Pen(Color.Black, 2);
+            popülasyon = new bool[panel1.Width / 10,panel1.Height / 10];
+            sonrakiP = new bool[panel1.Width / 10, panel1.Height / 10];
+            r = new Random();
+            sbs = new SolidBrush(Color.Black);
+            sbg = new SolidBrush(Color.Gray);
+            komşu = new List<bool>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!hesaplayıcı.IsBusy)
+                hesaplayıcı.RunWorkerAsync();
+        }
+
+        Graphics g;
+        bool[,] popülasyon,sonrakiP; //[x,y]
+        Pen k;
+        Random r;
+        SolidBrush sbs, sbg;
+        List<bool> komşu;
+
+        private void hesaplayıcı_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (Graphics f = CreateGraphics())
+            {
+                f.DrawRectangle(k, panel1.Location.X - 1, panel1.Location.Y - 1, panel1.Width + 2, panel1.Height + 2);
+            }
+            for (int i = 10; i < panel1.Width; i += 10)
+            {
+                g.DrawLine(k, i, 0, i, panel1.Height);
+            }
+            for (int i = 10; i < panel1.Height; i += 10)
+            {
+                g.DrawLine(k, 0, i, panel1.Width, i);
+            }
+
+
+            for (int x = 0; x < panel1.Width / 10; x++)
+            {
+                for (int y = 0; y < panel1.Height / 10; y++)
+                {
+                    if (r.Next(0,2) == 1)
+                    {
+                        popülasyon[x, y] = true;
+                        g.FillRectangle(sbs, new Rectangle(x * 10, y * 10, 9, 9));
+                    }
+                    else
+                    {
+                        g.FillRectangle(sbg, new Rectangle(x * 10, y * 10, 9, 9));
+                        popülasyon[x, y] = false;
+                    }
+                }
+            }
+
+
+            while (true)
+            {
+                for (int x = 0; x < panel1.Width / 10; x++)
+                {
+                    for (int y = 0; y < panel1.Height / 10; y++)
+                    {
+                        for (int i = -1; i < 2; i++)
+                        {
+                            for (int j = -1; j < 2; j++)
+                            {
+                                if (i == 0 && j == 0)
+                                    continue;
+                                try
+                                {
+                                    if (popülasyon[x + i, y + j])
+                                        komşu.Add(true);
+                                }
+                                catch
+                                {
+                                    continue;
+                                }
+                                
+                            }
+                        }
+
+                        if (!popülasyon[x, y])
+                        {
+                            if (komşu.Count == 3)
+                                sonrakiP[x, y] = true;
+                        }
+                        else
+                        {
+                            if(komşu.Count < 2 || komşu.Count > 3)
+                                sonrakiP[x, y] = false;
+                        }
+
+                        komşu.Clear();
+                    }
+                }
+
+                for (int x = 0; x < panel1.Width / 10; x++)
+                {
+                    for (int y = 0; y < panel1.Height / 10; y++)
+                    {
+                        if (sonrakiP[x, y])
+                            g.FillRectangle(sbs, new Rectangle(x * 10, y * 10, 9, 9));
+                        else
+                            g.FillRectangle(sbg, new Rectangle(x * 10, y * 10, 9, 9));
+                    }
+                }
+
+                popülasyon = sonrakiP;
+            }
+        }
+    }
+}
