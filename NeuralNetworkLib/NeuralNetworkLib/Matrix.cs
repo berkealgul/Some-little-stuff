6e090530d2074171bc9b69f1d3f3c45ead8811f6
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2019-03-19 20:42:38 +0200

    Add files via upload
    not tested properly

---
+++ NeuralNetworkLib/NeuralNetworkLib/Matrix.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace NeuralNetworkLib
+{
+    public class Matrix
+    {
+        public int rowCount;
+        public int columnCount;
+
+        public float[,] values; //actual matrix
+
+        public Matrix(int rowCount, int columnCount)
+        {
+            if (rowCount < 1 || columnCount < 1)
+                throw new ArgumentException("Matrises row or column count cant less than 1. Check constructor parameters");
+
+            this.columnCount = columnCount;
+            this.rowCount = rowCount;
+
+            values = new float[rowCount, columnCount];
+        }
+
+
+        public void Scale(float scaler)
+        {
+            for (int j = 0; j < rowCount; j++)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    values[j, i] *= scaler;
+                }
+            }
+        }
+
+
+        public void multply(Matrix otherMatrix)
+        {
+            if (columnCount != otherMatrix.columnCount || rowCount != otherMatrix.rowCount)
+                throw new InvalidOperationException("'otherMatrix' must have same dimensions with source matrix");
+
+            for (int j = 0; j < rowCount; j++)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    values[j, i] *= otherMatrix.values[j, i];
+                }
+            }
+        }
+
+
+        public void Add(Matrix otherMatrix)
+        {
+            if (columnCount != otherMatrix.columnCount || rowCount != otherMatrix.rowCount)
+                throw new InvalidOperationException("'otherMatrix' must have same dimensions with source matrix");
+
+            for (int j = 0; j < rowCount; j++)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    values[j, i] += otherMatrix.values[j, i];
+                }
+            }
+        }
+
+
+        public void Add_Num(float number)
+        {
+            for (int j = 0; j < rowCount; j++)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    values[j, i] += number;
+                }
+            }
+        }
+
+
+        public void Sub(Matrix otherMatrix)
+        {
+            otherMatrix.Scale(-1);
+            Add(otherMatrix);
+        }
+
+
+        public Matrix Transpose() // T(3x2) = 2x3
+        {
+            Matrix newMatrix = new Matrix(columnCount, rowCount);
+
+            for (int j = 0; j < newMatrix.rowCount; j++)
+            {
+                for (int i = 0; i < newMatrix.columnCount; i++)
+                {
+                    newMatrix.values[j, i] = values[i, j];
+                }
+            }
+
+            return newMatrix;
+        }
+
+
+        public static Matrix Product(Matrix firstMatrix, Matrix secMatrix) // m x n * n x a = m x a 
+        {
+            if (firstMatrix.columnCount != secMatrix.rowCount)
+                throw new InvalidOperationException("'firstMatrix's column count must equals 'secMatrix's row count");
+
+            Matrix newMatrix = new Matrix(firstMatrix.rowCount, secMatrix.columnCount);
+
+            for (int j = 0; j < newMatrix.rowCount; j++)
+            {
+                for (int i = 0; i < newMatrix.columnCount; i++)
+                {
+                    float[] row = new float[firstMatrix.columnCount];
+                    float[] column = new float[secMatrix.rowCount];
+
+                    for (int x = 0; x < row.Length; x++)
+                    {
+                        row[x] = firstMatrix.values[j, x];
+                        column[x] = secMatrix.values[x, i];
+                    }
+
+                    newMatrix.values[j, i] = dotproduct(row, column);
+                }
+            }
+
+            return newMatrix;
+        }
+
+
+        private static float dotproduct(float[] firstA, float[] secA)
+        {
+            if (firstA.Length != secA.Length)
+                throw new InvalidOperationException("arrays must have same lenght");
+
+            float result = 0;
+
+            for (int i = 0; i < firstA.Length; i++)
+            {
+                result += firstA[i] * secA[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NeuralNetworkLib/NeuralNetworkLib/NeuralNet.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace NeuralNetworkLib
+{
+    public class NeuralNet
+    {
+        Random random = new Random();
+
+        Matrix[] weights;
+        Matrix[] biases;
+
+        Matrix[] outputs_for_train;
+        Matrix[] inputs_for_train;
+
+        public NeuralNet(int input_neuron_count, int output_neuron_count, int hidden_neuron_count, int hiddenlayer_count)
+        {
+            weights = new Matrix[1 + hiddenlayer_count];
+            biases = new Matrix[1 + hiddenlayer_count];
+            outputs_for_train = new Matrix[1 + hiddenlayer_count];
+
+            //weight = matrix(next_layer_neuron_count x current_layer_neuron_count)
+            //bias = matrix(next_layer_neuron_count x 1)
+
+            weights[0] = new Matrix(hidden_neuron_count, input_neuron_count);
+            biases[0] = new Matrix(hidden_neuron_count, 1);
+
+            for (int i = 1; i < weights.Length - 1; i++)
+            {
+                weights[i] = new Matrix(hidden_neuron_count, hidden_neuron_count);
+                biases[i] = new Matrix(hidden_neuron_count, 1);
+            }
+
+            weights[weights.Length - 1] = new Matrix(output_neuron_count, hidden_neuron_count);
+            biases[weights.Length - 1] = new Matrix(output_neuron_count, 1);
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                randomize(weights[i]);
+                randomize(biases[i]);
+            }
+        }
+
+
+        public void gradient_decent_train(Matrix error, float learning_rate)
+        {
+            //dw = 2 * lr * E * (1 - O) * O * i_T 
+            //db = 2 * lr * E * (1 - O) * O 
+
+            for (int i = weights.Length - 1; i > -1; i--)
+            {
+                Matrix old_output = outputs_for_train[i];
+
+                error.Scale(2 * learning_rate);
+                old_output.Scale(-1);
+                old_output.Add_Num(1);
+                old_output.multply(old_output);
+                error.multply(old_output);
+
+                Matrix delta_biases = error;
+                Matrix inputs_T = inputs_for_train[i].Transpose();
+                Matrix delta_weights = Matrix.Product(error, inputs_T);
+
+                weights[i].Add(delta_weights);
+                biases[i].Add(delta_biases);
+
+            }
+        }
+
+
+        public Matrix Feedforward(Matrix input)
+        {
+            Matrix guess = input;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                inputs_for_train[i] = guess;
+
+                guess = Matrix.Product(weights[i], guess);
+                guess.Add(biases[i]);
+                Activate(guess);
+
+                outputs_for_train[i] = guess;
+            }
+
+            return guess;
+        }
+
+
+        void Activate(Matrix matrix) //sigmoid function is used here. Some other activation functions may required later on.
+        {
+            for (int j = 0, n = matrix.rowCount; j < n; j++)
+            {
+                for (int i = 0, m = matrix.columnCount; i < m; i++)
+                {
+                    matrix.values[j, i] = 1 / (1 + (float)Math.Exp(-matrix.values[j, i]));
+                }
+            }
+        }
+
+
+        void randomize(Matrix matrix)
+        {
+            for (int j = 0, n = matrix.rowCount; j < n; j++)
+            {
+                for (int i = 0, m = matrix.columnCount; i < m; i++)
+                {
+                    matrix.values[j, i] = (float)random.NextDouble();
+                }
+            }
+        }
+    }
+}
