0470e162d530a2c788ca6789d9ca903f4254aa81
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-12-03 22:43:43 +0300

    Add files via upload

---
+++ bruh step rotation/step_rotation/step_rotation.ino
@@ -0,0 +1,27 @@
+#include <AccelStepper.h>
+
+#define motorInterfaceType 1
+
+// Define pin connections & motor's steps per revolution
+const int dirPin = 2;
+const int stepPin = 3;
+// delay = 200 / 36 * 1000 = 180
+
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+}
+void loop()
+{
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(stepPin, LOW);
+  delay(90);
+}
